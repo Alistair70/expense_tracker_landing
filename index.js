2eef28dd2585d869e7ff83deeebc0b75e2143fea
Alistair70 <63516216+Alistair70@users.noreply.github.com>
2023-12-08 11:16:22 -0500

    expense tracker landing

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("toLogin").addEventListener("click", function() {
+    window.location.href = "https://expense-tracker-login.netlify.app";
+});
+document.getElementById("toSignup").addEventListener("click", function() {
+    window.location.href = "https://expense-tracker-signup.netlify.app";
+});
+
