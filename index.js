0ad5041b3a861d451a4bef36e7e1199293f9b231
Alistair70 <63516216+Alistair70@users.noreply.github.com>
2024-01-30 12:14:53 -0500

    added functionality to check for required cookies

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-document.getElementById("toLogin").addEventListener("click", function() {
+ document.getElementById("toLogin").addEventListener("click", function() {
     window.location.href = "https://expense-tracker-login.netlify.app";
 });
 document.getElementById("toSignup").addEventListener("click", function() {
     window.location.href = "https://expense-tracker-signup.netlify.app";
 });
 
-console.log('hi')
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch and display elements from the server
+    cookieCheck(); 
+
+});
+
+function cookieCheck()
+{
+    cookie_name = "expense_tracker_cookie_container"
+    if(document.cookie.split(';').some((item) => item.trim().startsWith(`${cookie_name}=`)))
+    {
+        console.log("true")
+        window.location.href = "https://expense-tracker-dash-board.netlify.app";
+    }
+}
